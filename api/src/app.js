30485cad955b949e38f6596f7ba1b474016a8c86
luizbrito6 <luizg1717@gmail.com>
2023-10-16 17:58:09 -0300

    fix: Database incorreto

--- api/src/app.js
+++ api/src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(router);
 
 const cors = require('cors')
-// CORS
+
 app.use((req, res, next) => {
     console.log('Api consumida!')
     res.header('Access-Control-Allow-Origin', '*')
