b7553b97d1b96340b3c3098a68d8966e98d50b51
luizbrito6 <luizg1717@gmail.com>
2023-10-16 16:44:55 -0300

    fix: Adicionando gitignore

--- api/src/app.js
+++ api/src/app.js
@@ -6,4 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
+const cors = require('cors')
+// CORS
+app.use((req, res, next) => {
+    console.log('Api consumida!')
+    res.header('Access-Control-Allow-Origin', '*')
+    app.use(cors())
+    next()
+})
+
 module.exports= app;
