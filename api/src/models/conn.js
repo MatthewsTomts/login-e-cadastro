51c6edd617d32923c72c963e41a67dd03ebe01db
violett <violett>
2023-10-16 15:24:29 -0300

    correção banco, adição gitignore

--- api/src/models/conn.js
+++ api/src/models/conn.js
@@ -4,7 +4,8 @@ const conn = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT || 3306
 });
 
 module.exports = conn;
