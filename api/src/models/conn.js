59046265ce84e10f73e2da9885997f9be039caa6
violett <violett>
2023-10-17 16:05:53 -0300

    fix: Cadastro Usuario

--- api/src/models/conn.js
+++ api/src/models/conn.js
@@ -4,7 +4,7 @@ const conn = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: process.env.MYSQL_DB,
     port: process.env.MYSQL_PORT || 3306
 });
 
