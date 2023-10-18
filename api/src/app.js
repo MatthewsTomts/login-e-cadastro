977968aba8a031b7f518c2f5fade20e8e2c1c5dc
luizbrito6 <luizg1717@gmail.com>
2023-10-18 11:54:28 -0300

    fix: CORS

--- api/src/app.js
+++ api/src/app.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const router = require("./router");
+require("dotenv").config();
 
-const app = express();
 
-app.use(express.json());
-app.use(router);
+const express = require("express")
+const router = require("./router")
+const cors = require("cors")
 
-const cors = require('cors')
-// CORS
-app.use((req, res, next) => {
-    console.log('Api consumida!')
-    res.header('Access-Control-Allow-Origin', '*')
-    app.use(cors())
-    next()
-})
+const app = express()
 
-module.exports= app;
+app.use(express.json())
+app.use(cors()) 
+
+app.use(router)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+
+
+module.exports= app
--- api/src/server.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
