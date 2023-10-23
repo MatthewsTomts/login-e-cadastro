0692fa4bc28419b1c0cb9fb9d73ec7ce59ecf3af
luizbrito6 <luizg1717@gmail.com>
2023-10-23 13:49:53 -0300

    fix: CORS

--- api/src/app.js
+++ api/src/app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const router = require("./router");
+const cors = require('cors')
 
 const app = express();
 
+
+app.use(cors());
 app.use(express.json());
+
 app.use(router);
 
-const cors = require('cors')
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    app.use(cors())
-    next()
-})
+
 
 module.exports= app;
