59cfca02cd0c9c5ee481156fdb375c04c7d55915
mayckl2 <mayckluciano2@gmail.com>
2023-10-24 14:45:02 -0300

    feat: tela de tarefas

---
+++ frontend/comp/tarefas.js
@@ -0,0 +1,40 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+  TextInput,
+} from "react-native";
+import { styles } from "../style.js";
+
+export default function Tarefas() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("../assets/logo.png")} style={styles.logo} />
+
+      <View style={styles.box}>
+        <Text style={styles.frase}>Minhas tarefas</Text>
+        <Text style={styles.subTituloTarefa}>
+          Crie, visualize e conclua suas tarefas
+        </Text>
+      </View>
+
+      <View style={styles.box}>
+        <TouchableOpacity style={styles.criarTarefa}>
+          <View style={{ flexDirection: "row", width: 120 }}>
+            <Text style={styles.txtCriarTarefa}>Criar nova tarefa</Text>
+            <Image
+              style={{ marginTop: "2%", marginLeft: "2%" }}
+              source={require("../assets/mais.png")}
+            />
+          </View>
+        </TouchableOpacity>
+
+        
+      </View>
+      <View style={styles.box}></View>
+    </View>
+  );
+}
